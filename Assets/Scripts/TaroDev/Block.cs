2f91d517e273aa5c6a3e5f73dafcc867802b16fb
Dylan Soungpanya <dsoungpa@ucsc.edu>
2022-10-12 05:45:50 -0700

    Tarodev P2 Finished

--- Assets/Scripts/TaroDev/Block.cs
+++ Assets/Scripts/TaroDev/Block.cs
@@ -6,6 +6,11 @@ using TMPro;
 public class Block : MonoBehaviour
 {
     public int Value;
+    public Node Node;
+    public Block MergingBlock;
+    public bool Merging;
+
+    public Vector2 Pos => transform.position;
     [SerializeField] private SpriteRenderer renderer;
     [SerializeField] private TextMeshPro text;
 
@@ -15,4 +20,25 @@ public class Block : MonoBehaviour
         renderer.color = type.Color;
         text.text = type.Value.ToString();
     }
+
+    public void SetBlock(Node node)
+    {
+        if(Node != null) Node.OccupiedBlock = null;
+        Node = node;
+        Node.OccupiedBlock = this;
+    }
+
+    public void MergeBlock(Block blockToMergeWith)
+    {
+        // Set the block we are merging with
+        MergingBlock = blockToMergeWith;
+
+        // Set current node as unoccupied to allow blocks to use it
+        Node.OccupiedBlock = null;
+
+        // Set the base block as merging, so it does not get used twice
+        blockToMergeWith.Merging = true;
+    }
+
+    public bool CanMerge(int value) => value == Value && !Merging && MergingBlock == null;
 }
--- Assets/Scripts/TaroDev/GameManager.cs
+++ Assets/Scripts/TaroDev/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using System.Security.Cryptography;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -15,6 +16,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Block blockPrefab;
     [SerializeField] private SpriteRenderer boardPrefab;
     [SerializeField] private List<BlockType> types;
+    [SerializeField] private float travelTime = 0.2f;
+    [SerializeField] private int winCondition = 2048;
+
+    [SerializeField] private GameObject winScreen, loseScreen;
 
     private List<Node> nodes;
     private List<Block> blocks;
@@ -30,6 +35,19 @@ public class GameManager : MonoBehaviour
         ChangeState(GameState.GenerateLevel);
     }
 
+    void Update()
+    {
+        if(state != GameState.WaitingInput) return;
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) Shift(Vector2.left);
+
+        if(Input.GetKeyDown(KeyCode.RightArrow)) Shift(Vector2.right);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)) Shift(Vector2.up);
+
+        if(Input.GetKeyDown(KeyCode.DownArrow)) Shift(Vector2.down);
+    }
+
     private void ChangeState(GameState newState)
     {
         state = newState;
@@ -39,27 +57,21 @@ public class GameManager : MonoBehaviour
             case GameState.GenerateLevel:
                 GenerateGrid();
                 break;
-
             case GameState.SpawningBlocks:
                 SpawnBlocks(round++ == 0 ? 2 : 1);
                 break;
-
             case GameState.WaitingInput:
 
                 break;
-
             case GameState.Moving:
 
                 break;
-
             case GameState.Win:
-
+                winScreen.SetActive(true);
                 break;
-
             case GameState.Lose:
-
-                break;
-            
+                loseScreen.SetActive(true);
+                break; 
             default:
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);      
         }
@@ -69,6 +81,8 @@ public class GameManager : MonoBehaviour
     {
         round = 0;
         nodes = new List<Node>();
+        blocks = new List<Block>();
+
         for(int x = 0; x < width; x++)
         {
             for(int y = 0; y < height; y++)
@@ -91,21 +105,115 @@ public class GameManager : MonoBehaviour
         // Get a list of nodes that are not Occupied by a block from the list of nodes
         var freeNodes = nodes.Where(n=>n.OccupiedBlock == null).OrderBy(b=>Random.value).ToList();
 
+        // For each of the free nodes get a certain amount of Nodes that you want to use
+        // for spawning the blocks
         foreach (var nodes in freeNodes.Take(amount))
         {
-            var block = Instantiate(blockPrefab, nodes.Pos, Quaternion.identity);    
-            block.Init(GetBlockTypeByValue(Random.value > 0.8f ? 4 : 2));
+            SpawnBlock(nodes, Random.value > 0.8f ? 4 : 2);
         }
 
-
-        
         if(freeNodes.Count() == 1)
         {
-            // Lost the game
+            ChangeState(GameState.Lose);
             return;
         }
 
+        // "b=>b" - "Is there any..."
+        ChangeState(blocks.Any(b=>b.Value == winCondition) ? GameState.Win : GameState.WaitingInput);
     }
+
+    void SpawnBlock(Node node, int value)
+    {
+        // Instantiate a block prefab at the chosen node location
+        var block = Instantiate(blockPrefab, node.Pos, Quaternion.identity);    
+
+        // Take the block game object and initialize it as a BlockType
+        block.Init(GetBlockTypeByValue(value));
+
+        // Assign the node to the Block and visa versa
+        block.SetBlock(node);
+
+        // Add block to the list
+        blocks.Add(block);
+    }
+
+    void Shift(Vector2 dir)
+    {
+        ChangeState(GameState.Moving);
+
+        // Order the block array by x then by y if it is moving left or down
+        // Or change it to ordered by y then x if it is moving right or up
+        var orderedBlocks = blocks.OrderBy(b => b.Pos.x).ThenBy(b => b.Pos.y).ToList(); 
+        if(dir == Vector2.right || dir == Vector2.up) orderedBlocks.Reverse();
+
+        // Go through the ordered list of blocks
+        foreach (var block in orderedBlocks)
+        {
+            // I think of "next" like current node that the block is on
+            var next = block.Node;
+            do {
+                // Make sure the current block sets this as the node
+                block.SetBlock(next);
+
+                // Get the next Node in the direction you want to move
+                var possibleNode = GetNodeAtPosition(next.Pos + dir);
+
+                //If there is a possible node in that direction
+                if(possibleNode != null)
+                {
+                    // We know a node is present
+                    // If its possible to merge, set merge
+                    if(possibleNode.OccupiedBlock != null && possibleNode.OccupiedBlock.CanMerge(block.Value))
+                    {
+                        block.MergeBlock(possibleNode.OccupiedBlock);
+                    }
+
+                    // Otherwise can we move to this spot?
+                    else if(possibleNode.OccupiedBlock == null) next = possibleNode;
+
+                    // None hit? End do while loop
+                }
+            } while (next != block.Node);
+               
+        }
+
+        var sequence = DOTween.Sequence();
+
+        foreach(var block in orderedBlocks)
+        {
+            var movePoint = block.MergingBlock != null ? block.MergingBlock.Node.Pos : block.Node.Pos;
+
+            sequence.Insert(0, block.transform.DOMove(movePoint, travelTime));
+        }
+
+        sequence.OnComplete(() => {
+            foreach (var block in orderedBlocks.Where(b=>b.MergingBlock != null))
+            {
+                MergeBlocks(block.MergingBlock, block);
+            }
+
+            ChangeState(GameState.SpawningBlocks);
+        });
+
+    }
+
+void MergeBlocks(Block baseBlock, Block mergingBlock)
+{
+    SpawnBlock(baseBlock.Node, baseBlock.Value * 2);
+    RemoveBlock(baseBlock);
+    RemoveBlock(mergingBlock);
+}
+
+void RemoveBlock(Block block)
+{
+    blocks.Remove(block);
+    Destroy(block.gameObject);
+}
+
+Node GetNodeAtPosition(Vector2 pos)
+{
+    return nodes.FirstOrDefault(n => n.Pos == pos);
+}
 
 }
 
